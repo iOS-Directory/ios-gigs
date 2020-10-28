5c2231f1e48e3459019e30d9f91c560b999277c6
fritzgt <tuanis@live.com>
2020-10-28 11:29:27 +0200

    create sign up

--- Gigs/Gigs/Model Controller/GigController.swift
+++ Gigs/Gigs/Model Controller/GigController.swift
@@ -9,10 +9,18 @@ import Foundation
 
 
 class GigController {
+    
     //MARK: - Properties
     var bearer: Bearer?
+    
     private let baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
     
+    //Use lazy because we are refering to baseURL which will not init till the class does
+    private lazy var signUpURL = URL(string: "/users/signup", relativeTo: baseURL)!
     
-    
+    func signUP(user:User,completion: @escaping (Error?) -> (Void))  {
+        
+        
+        
+    }
 }
