f24ba1da1445c6e8abf769b117ae1d05567d3d13
fritzgt <tuanis@live.com>
2020-10-28 09:53:45 +0200

    Create data models

---
+++ Gigs/Gigs/Model/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by FGT MAC on 10/27/20.
+//
+
+import Foundation
+
+struct Bearer {
+    var id: Int
+    var token: String
+    var uderId: Int
+}
---
+++ Gigs/Gigs/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by FGT MAC on 10/28/20.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var password: String
+}
